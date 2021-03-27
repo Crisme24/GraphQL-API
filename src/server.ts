ec9c204243f3dd71117325ab6367df2162f494db
jtrivino <jtrivino@webimpacto.es>
2021-03-27 19:57:04 +0100

    Go from Node Express to GraphQL and test it in browser

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@
 import express from 'express';
 import compression from 'compression';
 import cors from 'cors';
+import { IResolvers, makeExecutableSchema } from 'graphql-tools';
+import { GraphQLSchema } from 'graphql';
+import { graphqlHTTP } from 'express-graphql';
 
 const app = express();
 
@@ -9,12 +12,44 @@ app.use(cors());
 
 app.use(compression());
 
-app.use('/', (req, res) => {
+const typeDefs = `
+    type Query {
+        hola: String!
+        holaConNombre(nombre: String!): String!
+        holaAlCursoGraphQL: String!
+    }
+`;
+
+const resolvers: IResolvers = {
+    Query: {
+        hola(): string {
+            return 'Hola mundo';
+        },
+        holaConNombre(__: void, { nombre } ): string {
+            return `Hola mundo ${nombre}`;
+        },
+        holaAlCursoGraphQL(): string {
+            return 'Hola mundo al curso de GraphQL';
+        }
+    }
+};
+
+const schema: GraphQLSchema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
+
+/*app.use('/', (req, res) => {
     res.send('Welcome to course GraphQL');
-});
+});*/
+
+app.use('/', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
 
 const PORT = 5300;
 
 app.listen(
     { port: PORT},
-    () => console.log('\x1b[36m%s\x1b[0m', `Hola Mundo API GraphQL http://localhost:${PORT}`));
+    () => console.log('\x1b[36m%s\x1b[0m', `Hola Mundo API GraphQL http://localhost:${PORT}/graphql`));
